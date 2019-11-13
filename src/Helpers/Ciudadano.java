9d8af7a34dc87c8aba8c769aa09399d6dee10bde
marbinraudales <mraudalesfigueroa@gmail.com>
2019-11-12 22:26:01 -0600

    commit repositoro Modulo 4

---
+++ src/Helpers/Ciudadano.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Helpers;
+
+/**
+ *
+ * 
+ */
+public class Ciudadano {
+    private String Nombre;
+    private int Edad;
+    private int AñosExperiencia;
+
+    public String getNombre() {
+        return "Ever Carrasco";
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getEdad() {
+        return 21;
+    }
+
+    public void setEdad(int Edad) {
+        this.Edad = Edad;
+    }
+
+    public int getAñosExperiencia() {
+        return 5;
+    }
+
+    public void setAñosExperiencia(int AñosExperiencia) {
+        this.AñosExperiencia = AñosExperiencia;
+    }
+         
+    
+}
